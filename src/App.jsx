017c7e2d9c01ef3913e25bdf54900fd8b3d0bae7
denchik3192 <den465341@gmail.com>
2024-12-03 16:42:39 +0300

    feat: add header layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,58 @@
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { HStack } from '@chakra-ui/react';
+import { Drawer, HStack, Input } from '@chakra-ui/react';
+import { ColorModeButton } from './components/ui/color-mode';
+import { LuSearch } from 'react-icons/lu';
+import { InputGroup } from '@/components/ui/input-group';
+
+import {
+  DrawerActionTrigger,
+  DrawerBackdrop,
+  DrawerBody,
+  DrawerCloseTrigger,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <HStack>
+        <ColorModeButton />
+        <InputGroup flex="1" startElement={<LuSearch />}>
+          <Input placeholder="Search" />
+        </InputGroup>
         <Button>Click me</Button>
-        <Button>Click me</Button>
+        <DrawerRoot>
+          <DrawerBackdrop />
+          <DrawerTrigger asChild>
+            <Button variant="outline" size="sm">
+              Open Drawer
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Drawer Title</DrawerTitle>
+            </DrawerHeader>
+            <DrawerBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </p>
+            </DrawerBody>
+            <DrawerFooter>
+              <DrawerActionTrigger asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerActionTrigger>
+              <Button>Save</Button>
+            </DrawerFooter>
+            <DrawerCloseTrigger />
+          </DrawerContent>
+        </DrawerRoot>
       </HStack>
     </>
   );
